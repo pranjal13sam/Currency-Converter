c4fb40f9d66740d5724d669bce7133638b79aa53
Pranjal <pandeypranjal264@gmail.com>
2024-04-14 20:10:58 +0530

    updated api

--- src/hooks/useCurrencyinfo.js
+++ src/hooks/useCurrencyinfo.js
@@ -1,20 +1,17 @@
-import {useEffect ,useState} from 'react';
+import { useEffect,useState } from "react";
 
-function useCurrencyinfo(currency){
-  const [data,setData]=useState({})//empty use state kyuki agar fetch ni kar paaya toh block na hojaaye
+//writting custom hook:
+function useCurrencyInfo(currency){
+  const [data,setData]=useState({})//if api call nhi hua toh app crash na kar jaaye uske liye
   useEffect(()=>{
-    fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
     .then((res)=>res.json())
-      .then((res)=>setData(res[currency]))
-      
-    
-  },[currency])
-  console.log(data);
+    .then((res)=>setData(res[currency]))//yaha pe res[currency] issiliye lagaya hai kyuki hume ni pata ki url mei kya dena hai usd ya inr ya koi aur value
+    console.log(data)
+  },[currency])//jab jab currency ki value change hogi tab tab hum chahenge ki hamara ui fir se render ho
+  console.log(data)
   return data;
 }
 
-export default useCurrencyinfo;
 
-
-
-
+export default useCurrencyInfo;//method ko return kar diya hai takki data ka access le paaye
